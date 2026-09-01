63004764f8a97b004710204bb4e34813e398e9e8
ronaldsm <ronaldsm@cisco.com>
2026-09-01 17:57:41 -0600

    more updates

--- 2a_emotion_detection.py
+++ 2a_emotion_detection.py
@@ -5,7 +5,7 @@ def emotion_detector(text_to_analyse):
     # Define the URL for the sentiment analysis API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
-        # Create the payload with the text to be analyzed
+        # Create the payload with the text to be analised
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     # Set the headers with the required model ID for the API
